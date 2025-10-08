b0325c886c6c04bcd83aab2d80268ba60b240dfc
garehira <github@as.xerai.biz>
2025-10-08 17:23:04 +0200

    task6

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -1,5 +1,8 @@
 use std::error::Error;
+use axum::http::StatusCode;
+use axum::response::{Html, IntoResponse};
 use axum::Router;
+use axum::routing::{ post};
 use axum::serve::Serve;
 use tower_http::services::ServeDir;
 
@@ -14,7 +17,14 @@ pub struct Application {
 impl Application {
     pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
         let router = Router::new()
-                .nest_service("/", ServeDir::new("assets"));
+                .nest_service("/", ServeDir::new("assets"))
+                // .route("/", get(login))
+                .route("/signup", post(signup))
+                .route("/login", post(login))
+                .route("/verify-2fa", post(verify_2fa))
+                .route("/logout", post(logout))
+                .route("/verify-token", post(verify_token))
+                ;
 
         let listener = tokio::net::TcpListener::bind(address).await?;
         let address = listener.local_addr()?.to_string();
@@ -31,4 +41,21 @@ impl Application {
         println!("listening on {}", &self.address);
         self.server.await
     }
+}
+
+
+async fn login() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
+}
+async fn verify_2fa() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+async fn signup() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+async fn logout() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+async fn verify_token() -> impl IntoResponse {
+    StatusCode::OK.into_response()
 }
